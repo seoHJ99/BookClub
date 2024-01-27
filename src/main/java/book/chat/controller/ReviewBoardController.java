d2b599852f7972c34c52a7aef50c4a0d4196a21a
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-01-27 23:01:37 +0900

    Feat: 추가. 모든 리뷰 게시글 찾기

---
+++ src/main/java/book/chat/controller/ReviewBoardController.java
@@ -0,0 +1,24 @@
+package book.chat.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import book.chat.repository.ReviewBoardRepository;
+
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class ReviewBoardController {
+
+    private final ReviewBoardRepository reviewBoardRepository;
+
+    @RequestMapping("/reviews")
+    public String seeBoard(Model model){
+        log.info("board all= {}", reviewBoardRepository.findAll());
+        model.addAttribute("boards", reviewBoardRepository.findAll());
+        return "/board";
+    }
+}
