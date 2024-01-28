4483a18233f9c840de64c813958538861cc33830
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-01-28 21:55:21 +0900

    Feat: 추가. Review 검색 메서드 추가

--- src/main/java/book/chat/repository/ReviewBoardRepository.java
+++ src/main/java/book/chat/repository/ReviewBoardRepository.java
@@ -7,5 +7,9 @@ import java.util.List;
 public interface ReviewBoardRepository {
 
     List<ReviewDTO> findAll();
-
+    List<ReviewDTO> findAllByBookName(String name);
+    List<ReviewDTO> findAllByClubNo(Long no);
+    List<ReviewDTO> findAllByMember(Long no);
+    List<ReviewDTO> findAllByMember(String memberName);
+    List<ReviewDTO> findAllByReviewName(String name);
 }
--- src/main/java/book/chat/repository/ReviewBoardRepositoryImpl.java
+++ src/main/java/book/chat/repository/ReviewBoardRepositoryImpl.java
@@ -15,4 +15,29 @@ public class ReviewBoardRepositoryImpl implements ReviewBoardRepository {
     public List<ReviewDTO> findAll(){
         return new ArrayList<>(store.values());
     }
+
+    @Override
+    public List<ReviewDTO> findAllByBookName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<ReviewDTO> findAllByClubNo(Long no) {
+        return null;
+    }
+
+    @Override
+    public List<ReviewDTO> findAllByMember(Long no) {
+        return null;
+    }
+
+    @Override
+    public List<ReviewDTO> findAllByMember(String memberName) {
+        return null;
+    }
+
+    @Override
+    public List<ReviewDTO> findAllByReviewName(String name) {
+        return null;
+    }
 }
