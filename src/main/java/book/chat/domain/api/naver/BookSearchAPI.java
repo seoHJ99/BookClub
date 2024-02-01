99d93bc683efaf00d27f72ab8d1339d36327b1bf
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-01 22:55:02 +0900

    Feat: 추가. api요청시 display 옵션 추가. 메서드 분리.
    보여지는 검색 결과 개수를 의미하는 display 쿼리 추가.

--- src/main/java/book/chat/domain/api/naver/BookSearchAPI.java
+++ src/main/java/book/chat/domain/api/naver/BookSearchAPI.java
@@ -1,7 +1,5 @@
 package book.chat.domain.api.naver;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -18,28 +15,39 @@ import java.net.URLEncoder;
 @Component
 public class BookSearchAPI {
 
-    private final String BOOK_API = "https://openapi.naver.com/v1/search/book.json?query=";
+    private final String BOOK_URL = "https://openapi.naver.com/v1/search/book.json?query=";
 
-    public void bookSearch(String keyword) throws IOException {
-        
-        URL apiURL = new URL(BOOK_API + URLEncoder.encode( keyword ));
+    public void bookSearch(String keyword, Integer displayAmount) throws IOException {
+        URL apiURL = new URL(BOOK_URL + URLEncoder.encode( keyword ) + "&display=" + displayAmount);
+        String response = getResponse(sendRequest(apiURL));
+        log.info("response={}", response);
+    }
+
+    public void bookSearch(String keyword ) throws IOException {
+        URL apiURL = new URL(BOOK_URL + URLEncoder.encode( keyword ) );
+        String response = getResponse(sendRequest(apiURL));
+        log.info("response={}", response);
+    }
+
+    public String getResponse(HttpURLConnection connection) throws IOException {
+        InputStream inputStream = connection.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuffer stringBuffer = new StringBuffer();
+        String inputLine;
+        while ((inputLine = bufferedReader.readLine()) != null)  {
+            stringBuffer.append(inputLine);
+        }
+        bufferedReader.close();
+        String response = stringBuffer.toString();
+        return response;
+    }
+
+    public HttpURLConnection sendRequest(URL apiURL) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) apiURL.openConnection();
         connection.setRequestMethod("GET");
         connection.setRequestProperty("X-Naver-Client-Id", "jgYZagha6X71eBAcMkhS");
         connection.setRequestProperty("X-Naver-Client-Secret", "u8QcFEvsUD");
         connection.setDoOutput(true);
-
-        InputStream inputStream = connection.getInputStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        StringBuffer stringBuffer = new StringBuffer();
-        String inputLine;
-
-        while ((inputLine = bufferedReader.readLine()) != null)  {
-            stringBuffer.append(inputLine);
-        }
-        bufferedReader.close();
-
-        String response = stringBuffer.toString();
-        log.info("response={}", response);
+        return connection;
     }
 }
