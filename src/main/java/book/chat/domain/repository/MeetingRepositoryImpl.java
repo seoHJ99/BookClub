f653d5c3a338db6605cbc80fc767b09b4923b08c
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-01 23:26:38 +0900

    Feat: 추가. MeetingRepositoryImpl 상속받음

--- src/main/java/book/chat/domain/repository/MeetingRepositoryImpl.java
+++ src/main/java/book/chat/domain/repository/MeetingRepositoryImpl.java
@@ -1,8 +1,31 @@
 package book.chat.domain.repository;
 
+import book.chat.domain.DTO.MeetingDto;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class MeetingRepositoryImpl {
+import java.time.LocalDate;
+import java.util.List;
 
+@Repository
+public class MeetingRepositoryImpl implements MeetingRepository{
+
+    @Override
+    public List<MeetingDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<MeetingDto> findAllByDate(LocalDate meetingDate) {
+        return null;
+    }
+
+    @Override
+    public List<MeetingDto> findAllByClub(long clubNo) {
+        return null;
+    }
+
+    @Override
+    public MeetingDto findByNo(long meetingNo) {
+        return null;
+    }
 }
