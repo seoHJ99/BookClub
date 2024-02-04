23ad3877045829cce9c01477960e460b672c4345
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-04 16:09:11 +0900

    Feat: 추가. Redis 저장 확인 테스트코드
    score 가 올라가서 랭킹이 정해지는 redis 코드 테스트

--- src/test/java/book/chat/domain/redis/RedisTest.java
+++ src/test/java/book/chat/domain/redis/RedisTest.java
@@ -13,6 +13,9 @@ public class RedisTest {
 
     private RedisTestRepository repository;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @BeforeEach
     void init(){
         repository = new RedisTestRepository();
@@ -31,5 +34,48 @@ public class RedisTest {
 
         assertThat(findEntity.isPresent()).isEqualTo(Boolean.TRUE);
         assertThat(findEntity.get()).isEqualTo(new RedisTestEntity(1L, "aaa"));
+    }
+
+    @Test
+    public void plusPoint(){
+        redisTemplate.opsForZSet().removeRange("test",0,-1);
+
+        TestRedis dto = new TestRedis("처음");
+        TestRedis dto2 = new TestRedis("처음");
+        TestRedis dto3 = new TestRedis("두번째");
+        TestRedis dto4 = new TestRedis("세번째");
+        saveRedisData(dto);
+        saveRedisData(dto2);
+        saveRedisData(dto3);
+        saveRedisData(dto4);
+
+        assertThat(redisTemplate.opsForZSet().reverseRange("test",0,-1).toString())
+                .isEqualTo("[처음, 세번째, 두번째]");
+
+    }
+
+    private void saveRedisData(TestRedis dto) {
+        ZSetOperations zSetOperations = redisTemplate.opsForZSet();
+        Set popularBooks = zSetOperations.reverseRange("test", 0, -1);
+        if (!popularBooks.contains(dto.getName())) {
+            redisTemplate.opsForZSet().add("test", dto.getName().toString(), 1);
+        } else {
+            System.out.println("여기");
+            redisTemplate.opsForZSet().incrementScore("test", dto.getName().toString(), +1);
+        }
+        popularBooks = zSetOperations.reverseRange("test", 0, -1);
+        System.out.println("popularBooks = " + popularBooks);
+    }
+
+    static class TestRedis{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public TestRedis(String name) {
+            this.name = name;
+        }
     }
 }
