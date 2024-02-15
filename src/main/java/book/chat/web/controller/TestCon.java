49d27684c6fc778f7f93fba00dfe026c7a13967a
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-15 19:13:13 +0900

    Docs: 추가. 타임리프 레이아웃 적용 home화면

---
+++ src/main/java/book/chat/web/controller/TestCon.java
@@ -0,0 +1,14 @@
+package book.chat.web.controller;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestCon {
+
+    @GetMapping("/tamplate/test")
+    public String test(){
+        return "layout/home";
+    }
+}
