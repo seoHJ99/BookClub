b58c7b6939e35f54a646eb5226f0f4058a4a16e1
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-18 15:47:36 +0900

    Feat: 추가. 책 dto 추가

---
+++ src/main/java/book/chat/web/DTO/Book.java
@@ -0,0 +1,12 @@
+package book.chat.web.DTO;
+
+public class Book {
+    private String name;
+    private String shoppingLink;
+    private String image;
+    private String author;
+    private String publisher;
+    private String isbn;
+    private String publishDate;
+    private String description;
+}
