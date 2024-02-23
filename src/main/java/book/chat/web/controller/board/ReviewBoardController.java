7f89bd8c943c8fc6965776cb21b34722f88bd314
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-23 21:44:48 +0900

    Feat: 추가. 게시글 리스트 조회. 댓글 저장. 글 저장시 redis에 isbn저장

--- src/main/java/book/chat/web/controller/board/ReviewBoardController.java
+++ src/main/java/book/chat/web/controller/board/ReviewBoardController.java
@@ -1,23 +1,14 @@
 package book.chat.web.controller.board;
 
 import book.chat.domain.api.naver.BookSearchAPI;
-import book.chat.domain.entity.Comment;
-import book.chat.domain.entity.Review;
-import book.chat.domain.repository.ReviewBoardRepository;
 import book.chat.domain.service.CommentService;
 import book.chat.domain.service.ReviewBoardService;
 import book.chat.web.DTO.BookDTO;
 import book.chat.web.DTO.CommentDTO;
 import book.chat.web.DTO.ReviewDTO;
-import book.chat.web.SessionConst;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.parser.ParseException;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -25,11 +16,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 
 @Controller
@@ -42,45 +31,57 @@ public class ReviewBoardController {
     private final BookSearchAPI bookSearchAPI;
     private final CommentService commentService;
 
-    @GetMapping("/")
-    public String reviewBoard(Model model, @RequestParam("no") Long no){
-        model.addAttribute("review", reviewBoardService.findReviewByNo(no));
+    @GetMapping("")
+    public String reviewBoard(Model model, @RequestParam("no") Long no) {
+        ReviewDTO review = reviewBoardService.findReviewByNo(no);
+        model.addAttribute("review", review);
+        model.addAttribute("comment", new CommentDTO());
+        model.addAttribute("book", bookSearchAPI.bookSearch(review.getIsbn()).get(0));
+        model.addAttribute("comments", commentService.findByBoardNo(no));
         return "layout/board-main";
     }
 
 
     @GetMapping("/list")
-    public String allReview(Model model){
-        model.addAttribute("boards", reviewBoardService.findAllRecent());
+    public String allReview(Model model) {
+        List<ReviewDTO> rescent10Dtos = reviewBoardService.findAllRecent();
+        List<BookDTO> bookDTOS = rescent10Dtos.stream()
+                .map(dto -> bookSearchAPI.bookSearch(dto.getIsbn()).get(0))
+                .collect(Collectors.toList());
+//        List<String> images = rescent10Dtos.stream()
+//                .map(dto -> bookSearchAPI.getBookImg(dto.getIsbn()))
+//                .collect(Collectors.toList());
+        model.addAttribute("books", bookDTOS);
+        model.addAttribute("boards", rescent10Dtos);
         return "layout/board-list";
     }
 
+
     @GetMapping("/save")
-    public String writeForm(@ModelAttribute("review") ReviewDTO review){
+    public String writeForm(@ModelAttribute("review") ReviewDTO review) {
         return "layout/board-write";
     }
 
     @PostMapping("/save")
     public String saveReview(@Validated @ModelAttribute("review") ReviewDTO review, BindingResult bindingResult) throws IOException, ParseException {
-        if(bindingResult.hasErrors()){
-            log.info("error={}", bindingResult);
+        if (bindingResult.hasErrors()) {
             return "layout/board-write";
         }
         review.setWriteDate(LocalDateTime.now());
 //        review.setWriter((String) request.getSession(false).getAttribute(SessionConst.LOGIN_MEMBER));
-        review.setBook(bookSearchAPI.bookSearch(review.getIsbn()).get(0));
+//        String bookImg = bookSearchAPI.getBookImg(review.getIsbn());
+//        review.setBookImg(bookImg);
+        review.setIsbn(bookSearchAPI.bookSearch(review.getIsbn()).get(0).getIsbn());
         reviewBoardService.saveReview(review);
-        return "redirect: /review?no="+review.getNo();
+        return "redirect: /review?no=" + review.getNo();
     }
 
     @PostMapping("/comment/save")
-    public String saveComment(@Validated @ModelAttribute CommentDTO commentDTO,
+    public String saveComment( @Validated @ModelAttribute("comment") CommentDTO comment,
                               BindingResult bindingResult,
-                              Model model){
-        commentService.save(commentDTO);
-        ReviewDTO review = reviewBoardService.findReviewByNo(commentDTO.getBoardNo());
-        model.addAttribute("review", review);
-        model.addAttribute("comments", commentService.findByBoardNo(commentDTO.getBoardNo()));
-        return "redirect: /review?no="+review.getNo();
+                              Model model) {
+        commentService.save(comment);
+        ReviewDTO review = reviewBoardService.findReviewByNo(comment.getBoardNo());
+        return "redirect:/review?no=" + review.getNo();
     }
 }
