d6d646df11dbf5f9000feb15c5c9dfc33ff41e17
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-23 21:45:09 +0900

    Feat: 추가. 댓글 service 추가.

--- src/main/java/book/chat/domain/service/CommentService.java
+++ src/main/java/book/chat/domain/service/CommentService.java
@@ -6,6 +6,8 @@ import book.chat.web.DTO.CommentDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,11 +18,17 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     public void save(CommentDTO commentDTO){
+        // todo writerId 로그인 구현후 삽입
+        commentDTO.setDate(LocalDate.now());
+        commentDTO.setTime(LocalTime.now());
+        commentDTO.setWriterId("aaa");
         commentRepository.save(new Comment(commentDTO));
     }
 
     public List<CommentDTO> findByBoardNo(Long no){
-        List<Comment> entities = commentRepository.findByBoardNo(no);
+        System.out.println("CommentService.findByBoardNo");
+        List<Comment> entities = commentRepository.findByIdBoardNo(no);
+        System.out.println(entities.size());
         return entities.stream()
                 .map(entity -> new CommentDTO(entity))
                 .collect(Collectors.toList());
