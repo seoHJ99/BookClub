d80b83a9c2664f24cbe41d1427e92943dc09373e
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-23 20:55:49 +0900

    Feat: 수정. book 대신 책 isbn으로 변경

--- src/main/java/book/chat/domain/entity/Review.java
+++ src/main/java/book/chat/domain/entity/Review.java
@@ -4,6 +4,7 @@ import book.chat.web.DTO.BookDTO;
 import book.chat.web.DTO.ReviewDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @Table(name = "review")
+@NoArgsConstructor
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +29,6 @@ public class Review {
         this.content = reviewDTO.getContent();
         this.writeDate = reviewDTO.getWriteDate();
         this.writer = reviewDTO.getWriter();
-//        this.book = reviewDTO.getBook();
+        this.book= reviewDTO.getIsbn();
     }
 }
