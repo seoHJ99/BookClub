ec70fc847f2cdaefed8069b092397d207addaf04
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-23 21:48:31 +0900

    Feat: 수정. pk컬럼이 따로 없는 entity 클래스에 맞게 수정

--- src/main/java/book/chat/web/DTO/CommentDTO.java
+++ src/main/java/book/chat/web/DTO/CommentDTO.java
@@ -2,24 +2,27 @@ package book.chat.web.DTO;
 
 import book.chat.domain.entity.Comment;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Data
+@NoArgsConstructor
 public class CommentDTO {
     private Long boardNo;
     private String writerId;
     private String content;
     private LocalDate date;
-    private Time time;
+    private LocalTime time;
 
     public CommentDTO(Comment entity) {
-        this.boardNo = entity.getBoardNo();
-        this.writerId = entity.getWriterId();
+        this.boardNo = entity.getId().getBoardNo();
+        this.writerId = entity.getId().getWriterId();
         this.content = entity.getContent();
-        this.date = entity.getDate();
-        this.time = entity.getTime();
+        this.date = entity.getId().getDate();
+        this.time = entity.getId().getTime();
     }
 }
