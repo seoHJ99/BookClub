a58b92a23d184803b4ece76cb75f836466e165fb
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-24 23:55:20 +0900

    Feat: 추가. 홈 페이지 보여주기

--- src/main/java/book/chat/web/controller/board/HomeController.java
+++ src/main/java/book/chat/web/controller/board/HomeController.java
@@ -1,13 +1,20 @@
 package book.chat.web.controller.board;
 
+import book.chat.domain.api.naver.BookSearchAPI;
 import book.chat.domain.service.ClubService;
 import book.chat.domain.service.MemberService;
 import book.chat.domain.service.RedisService;
 import book.chat.domain.service.ReviewBoardService;
+import book.chat.web.DTO.BookDTO;
+import book.chat.web.DTO.ReviewDTO;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,12 +24,20 @@ public class HomeController {
     private final ReviewBoardService reviewBoardService;
     private final RedisService redisService;
     private final MemberService memberService;
+    private final BookSearchAPI bookSearchAPI;
+
+
 
     @GetMapping("/")
     public String home(Model model){
+        List<ReviewDTO> rescent10Dtos = reviewBoardService.findRecent10Review();
+        List<BookDTO> bookDTOS = rescent10Dtos.stream()
+                .map(dto -> bookSearchAPI.bookSearch(dto.getIsbn()).get(0))
+                .collect(Collectors.toList());
+        model.addAttribute("books", bookDTOS);
         model.addAttribute("clubs",clubService.findRecent4Club());
-        model.addAttribute("reviews", reviewBoardService.findRecent10Review());
-        model.addAttribute("populars", redisService.getTop10PopularBooks());
+        model.addAttribute("boards", rescent10Dtos);
+        model.addAttribute("ranking", redisService.getTop10PopularBooks());
         return "layout/home";
     }
 
