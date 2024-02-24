4a1533553ee0ccec4ae965b2d1830f7f8f9b338c
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-24 23:23:20 +0900

    Feat: 추가. reids에서 탑10책 가져오기

--- src/main/java/book/chat/domain/service/RedisService.java
+++ src/main/java/book/chat/domain/service/RedisService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 @Slf4j
@@ -19,14 +21,16 @@ public class RedisService {
 
     private final RedisTemplate redisTemplate;
 
+
+    // 회원가입시 중복 검사 체크.
     @Transactional
-    public void idDuplicationSave(String id){
-        if(!existId(id)){
+    public void idDuplicationSave(String id) {
+        if (!existId(id)) {
             redisTemplate.opsForValue().set(id.hashCode(), id, Duration.ofMinutes(5));
         }
     }
 
-    public boolean existId(String id){
+    public boolean existId(String id) {
         return Boolean.TRUE.equals(redisTemplate.opsForValue().get(id.hashCode()));
     }
 
@@ -41,10 +45,21 @@ public class RedisService {
         }
     }
 
+    public List<String> getTop10PopularBooks() {
+        ZSetOperations zSetOperations = redisTemplate.opsForZSet();
+        String popularBooks = zSetOperations.reverseRange("popularBooks", 0, -1).toString();
+        List<String> listBooks = Arrays.stream(popularBooks
+                        .replaceAll("\\[", "")
+                        .replaceAll("]", "")
+                        .split(","))
+                .toList();
+        return listBooks;
+    }
+
     @Transactional
     @Scheduled(cron = "0 0 0 * * ?")
     public void resetBookPopular() {
-        redisTemplate.opsForZSet().removeRange("popularBooks",0,-1);
+        redisTemplate.opsForZSet().removeRange("popularBooks", 0, -1);
         log.info("매일 자정 화제의 책 초기화");
     }
 }
