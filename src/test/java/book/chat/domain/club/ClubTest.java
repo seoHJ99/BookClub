be052c7a9b2c23b152dc0470636f8083df7abbbe
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-26 00:13:21 +0900

    Test: 추가. map과 limit 를 동시 사용할때 map내부 함수 실행 횟수 테스트
    길이가 긴 list를 stream으로 변환하여 map과 limit 함수 적용시, map 함수 실행 횟수 확인.

---
+++ src/test/java/book/chat/domain/club/ClubTest.java
@@ -0,0 +1,31 @@
+package book.chat.domain.club;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+//@SpringBootTest
+public class ClubTest {
+
+//    @Autowired
+//    ClubService clubService;
+
+    @Test
+    void streamLimitTest() {
+        List<Integer> list = new ArrayList<>(100);
+        for (int i = 0; i < 100; i++) {
+            list.add(i);
+        }
+        list= list.stream()
+                .map(entity -> {
+                    System.out.println(entity);
+                    return 0;
+                }).limit(10)
+                .collect(Collectors.toList());
+        Assertions.assertThat(list.size()).isEqualTo(10);
+    }
+}
