4e786f02c8da007093356950654ff2b193804923
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-25 23:12:51 +0900

    Feat: 추가. 스프링 jpa 적용

--- src/main/java/book/chat/domain/repository/MemberRepository.java
+++ src/main/java/book/chat/domain/repository/MemberRepository.java
@@ -3,18 +3,23 @@ package book.chat.domain.repository;
 import book.chat.web.DTO.MemberDTO;
 import book.chat.domain.entity.Member;
 import book.chat.web.DTO.MemberJoinForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository {
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Member save(MemberJoinForm joinForm);
-    Member update(MemberDTO memberDTO);
+    Member save(Member joinForm);
+//    Member update(MemberDTO memberDTO);
 
     Optional<Member> findById(String id);
 
-    Optional<Member> findBySessionKey(String id);
+//    Optional<Member> findBySessionKey(String id);
 
-    List<Member> findByClubNo(Long clubNo);
+//    List<Member> findByJoinClubIn(Long clubNo);
+
+    List<Member> findByNoIn(List<Long> memberNos);
 }
