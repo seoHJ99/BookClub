4073bd6c1e1c7c2300394ae8eace209d09147ebd
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-25 23:58:54 +0900

    Test: 추가. 한번에 책 검색 한도 찾기 테스트.

--- src/test/java/book/chat/domain/api/naver/BookSearchAPITest.java
+++ src/test/java/book/chat/domain/api/naver/BookSearchAPITest.java
@@ -1,28 +1,43 @@
 package book.chat.domain.api.naver;
 
 import book.chat.web.DTO.BookDTO;
-import org.assertj.core.api.Assertions;
 import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BookSearchAPITest {
+    BookSearchAPI api;
 
-    @Test
-    void bookSearch() throws IOException, ParseException {
-        BookSearchAPI api = new BookSearchAPI();
-        List<BookDTO> list = api.bookSearch("움베르토 에코", 10, 1);
-        Assertions.assertThat(list.get(0).getAuthor()).isEqualTo("움베르토 에코");
+    @BeforeEach
+    void init() {
+        api = new BookSearchAPI();
     }
 
     @Test
-    void isbnBookSearch() throws IOException, ParseException {
-        BookSearchAPI api = new BookSearchAPI();
+    void bookSearch() throws IOException, ParseException {
+        List<BookDTO> list = api.bookSearch("움베르토 에코", 10, 1);
+        assertThat(list.get(0).getAuthor()).isEqualTo("움베르토 에코");
+    }
+
+    @Test
+    void isbnBookSearch() {
         List<BookDTO> list = api.bookSearch("9791192300818");
-        Assertions.assertThat(list.get(0).getName()).contains("마흔에 읽는 쇼펜하우어");
+        assertThat(list.get(0).getName()).contains("마흔에 읽는 쇼펜하우어");
+    }
+
+    @Test
+    void findBooksSearchLimit() {
+        assertThatThrownBy(() -> {
+            for (int i = 0; i < 100; i++) {
+                api.bookSearch("9791170222743");
+                System.out.println(i);
+            }
+        }).isInstanceOf(Exception.class);
     }
 }
