d3aeac012b4edc7c036fcd7e168aa8d871ada7df
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-27 19:33:11 +0900

    Test: 추가. 필드 리스트 getter 을 통한 요소 추가

--- src/test/java/book/chat/domain/club/ClubTest.java
+++ src/test/java/book/chat/domain/club/ClubTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,5 +24,24 @@ public class ClubTest {
                 }).limit(10)
                 .collect(Collectors.toList());
         Assertions.assertThat(list.size()).isEqualTo(10);
+    }
+
+    @Test
+    void saveList(){
+        TestList test = new TestList();
+        test.getList().add(5L);
+        Assertions.assertThat(test.getList()).isEqualTo(new ArrayList<>(Arrays.asList(new Long[]{1L,2L,3L,4L,5L})));
+    }
+
+    private class TestList{
+        private List<Long> list = new ArrayList<>(Arrays.asList(new Long[]{1L,2L,3L,4L}));
+
+        public List<Long> getList() {
+            return list;
+        }
+
+        public void setList(List<Long> list) {
+            this.list = list;
+        }
     }
 }
