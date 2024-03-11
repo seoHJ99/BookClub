21d284a3b9b4cfd50218a4d78b66e8b60d899ea0
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-03-11 14:31:58 +0900

    Feat: 추가. 로그인 확인용 aop 추가

--- src/main/java/book/chat/BookClubApplication.java
+++ src/main/java/book/chat/BookClubApplication.java
@@ -3,11 +3,13 @@ package book.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@EnableAspectJAutoProxy
 public class BookClubApplication {
 
 	public static void main(String[] args) {
