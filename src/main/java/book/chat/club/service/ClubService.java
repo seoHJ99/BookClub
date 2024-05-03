46fb1d096c59c60ba929683e12e404a7c89a7f67
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-05-04 07:56:03 +0900

    Docs: 추가. ClubService 주석 작성

--- src/main/java/book/chat/club/service/ClubService.java
+++ src/main/java/book/chat/club/service/ClubService.java
@@ -29,10 +29,21 @@ public class ClubService {
     private final AwsS3Service awsS3Service;
 
 
+    /**
+     * [클럽 번호로 클럽 찾기]
+     * @param clubNo (클럽 번호)
+     * @return clubDTO (클럽 DTO )
+     * */
     public ClubDTO findClubByNo(Long clubNo) {
         return new ClubDTO(clubRepository.findByNo(clubNo).orElse(null));
     }
 
+    /**
+     * [새로운 클럽 db저장]
+     * @param newClub (새롭게 만들 클럽 dto)
+     * @param leader (클럽을 만드는 클럽장)
+     * @return 새롭게 만들어진 클럽 (clubDTO)
+     * */
     @Transactional
     public ClubDTO save(ClubMakingForm newClub, MemberDTO leader) {
         String url = "https://image.utoimage.com/preview/cp872722/2022/12/202212008462_500.jpg";
@@ -46,14 +57,27 @@ public class ClubService {
         return new ClubDTO(savedEntity);
     }
 
+    /**
+     * [모든 클럽 찾기]
+     * @return 모든 클럽 리스트 (List)
+     * */
     public List<ClubDTO> findAll() {
         return transferToDtoList(clubRepository.findAll());
     }
 
+    /**
+     * [최근 만들어진 클럽 4개 찾기]
+     * @return 최근 개설된 4개 클럽 리스트 (List)
+     * */
     public List<ClubDTO> findRecent4Club() {
         return transferToDtoList(clubRepository.findTop4ByOrderByStartDateDesc());
     }
 
+    /**
+     * [모임 맴버 찾기]
+     * @param clubDTO (모임 dto)
+     * @return 모임 맴버 리스트 (List)
+     * */
     public List<MemberDTO> findClubMember(ClubDTO clubDTO) {
         List<Long> memberList = clubDTO.getMembers();
         List<Member> clubMember = memberRepository.findByNoIn(memberList);
@@ -62,6 +86,13 @@ public class ClubService {
                 .collect(Collectors.toList());
     }
 
+
+    /**
+     * [모임에서 최근 읽은 책 10권을 네이버 api를 이용해 정보를 가져옴]
+     * @param bookList (모임에서 읽은 책 리스트)
+     * @return 네이버 책 검색 api를 이용해 찾은 10개의 책 검색 결과 (List)
+     * @see BookSearchAPI#bookSearch(String keyword)
+     * */
     public List<BookDTO> findReadBooksLimit10(List<String> bookList) {
         if (bookList.size() == 0) {
             return null;
@@ -89,18 +120,33 @@ public class ClubService {
         return books;
     }
 
+    /**
+     * [클럽 엔티티 리스트 => 클럽dto 리스트로 전환]
+     * @param entities (클럽 엔티티 list)
+     * @return 클럽dto 리스트 (List)
+     * */
     public List<ClubDTO> transferToDtoList(List<Club> entities) {
         return entities.stream()
                 .map(entity -> new ClubDTO(entity))
                 .collect(Collectors.toList());
     }
 
+    /**
+     * [클럽 정보 업데이트]
+     * @param clubDTO (업데이트할 클럽 정보)
+     * @return 업데이트 된 clubDTO (ClubDTO)
+     * */
     @Transactional
     public ClubDTO updateClubInfo(ClubDTO clubDTO) {
         clubRepository.save(new Club(clubDTO));
         return clubDTO;
     }
 
+    /**
+     * [클럽에 신규 맴버 가입]
+     * @param member (가입하려는 사용자 dto)
+     * @param clubNo (가입하려는 모임 번호)
+     * */
     @Transactional
     public void joinMember(MemberDTO member, Long clubNo) {
         ClubDTO club = findClubByNo(clubNo);
