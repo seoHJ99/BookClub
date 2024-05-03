2b9cdc20dd82ffe33e27d3609a6555f0a315f13d
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-05-03 21:29:28 +0900

    Docs: 추가. CommentService 주석 작성

--- src/main/java/book/chat/board/service/CommentService.java
+++ src/main/java/book/chat/board/service/CommentService.java
@@ -22,6 +22,11 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final ClubCommentRepository clubCommentRepository;
 
+    /**
+     * [리뷰 댓글 저장]
+     * @param commentDTO (저장할 댓글 dto)
+     */
+
     @Transactional
     public void save(CommentDTO commentDTO) {
         commentDTO.setDate(LocalDate.now());
@@ -29,22 +34,35 @@ public class CommentService {
         commentRepository.save(new Comment(commentDTO));
     }
 
+    /**
+     * [회원이 작성한 모든 클럽 게시글에 대한 댓글 찾기]
+     * @param id (회원 id)
+     * @return List (클럽 게시글 댓글 리스트)
+     */
     public List<ClubCommentDTO> findClubCommentById(String id) {
         List<ClubCommentDTO> dtoList = clubCommentRepository.findAllByIdWriterIdOrderByIdDateDesc(id).stream()
                 .map(ClubCommentDTO::new)
                 .collect(Collectors.toList());
-        for (ClubCommentDTO clubCommentDTO : dtoList) {
-            System.out.println(clubCommentDTO);
-        }
         return dtoList;
     }
 
+    /**
+     * [리뷰 번호와 댓글 번호로 댓글 찾기]
+     * @param boardNo (리뷰 번호)
+     * @param commentNo (리뷰에 대한 댓글 번호)
+     * @return CommentDTO (댓글 dto)
+     * */
     public CommentDTO findByBoardNoAndCommentNo(Long boardNo, Long commentNo){
         Comment entity = commentRepository.findByIdBoardNoAndIdNo(boardNo, commentNo);
         CommentDTO dto = new CommentDTO(entity);
         return dto;
     }
 
+    /**
+     * [특정한 리뷰에 달린 모든 댓글 찾기]
+     * @param no (리뷰 번호)
+     * @return List (댓글 dto 리스트)
+     * */
     public List<CommentDTO> findByBoardNo(Long no) {
         List<Comment> entities = commentRepository.findByIdBoardNoOrderByIdDate(no);
         return entities.stream()
@@ -57,11 +75,22 @@ public class CommentService {
 //        return entity.stream().map(CommentDTO::new).collect(Collectors.toList());
 //    }
 
+    /**
+     * [사용자가 작성한 모든 댓글 찾기]
+     * @param memberId (작성자 id)
+     * @return List (댓글 dto 리스트)
+     * */
     public List<CommentDTO> findByWriter(String memberId) {
         List<Comment> entity = commentRepository.findWithBoardByWriterIdOrderByIdDate(memberId);
         return entity.stream().map(CommentDTO::new).collect(Collectors.toList());
     }
 
+    /**
+     * [특정 댓글 삭제]
+     * @param commentDTO (삭제할 댓글 dto)
+     * @param memberDTO (이 요청을 한 사용자 dto)
+     * @return boolean (삭제되면 true, 안되면 false 반환)
+     * */
     public boolean delete(CommentDTO commentDTO, MemberDTO memberDTO) {
         if (commentDTO.getWriterId().equals(memberDTO.getId())) {
             commentRepository.deleteById(commentDTO.getCommentId());
@@ -70,6 +99,10 @@ public class CommentService {
         return false;
     }
 
+    /**
+     * [특정 리뷰의 모든 댓글 삭제]
+     * @param no (리뷰 번호)
+     * */
     public void deleteAllBoardComment(Long no){
         commentRepository.deleteAllByIdBoardNo(no);
     }
